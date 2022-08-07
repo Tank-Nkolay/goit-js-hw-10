a5b5e748ac13b228741c145abe6ed58d096b377a
Nikolay Tankov <tankov.n.k@gmail.com>
2022-08-07 19:51:07 +0300

    task-2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+let parsedCurrentTime = 0;
+
+player.on('timeupdate', throttle(onPlay, 500));
+
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+function onPlay(evt) {
+  localStorage.setItem(LOCALSTORAGE_KEY, evt.seconds);
+}
+const currentTime = localStorage.getItem(LOCALSTORAGE_KEY);
+
+function timeHandler(currentTime) {
+  if (currentTime) {
+    parsedCurrentTime = JSON.parse(currentTime);
+  } else {
+    parsedCurrentTime = 0;
+  }
+}
+
+timeHandler(currentTime);
+
+player.setCurrentTime(parsedCurrentTime);
