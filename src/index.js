e885ac2b3c963d30eefc70b9e98307d1108ca21e
Nikolay Tankov <tankov.n.k@gmail.com>
2022-08-22 21:46:00 +0300

    1

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ const countryInfo = document.querySelector('.country-info');
 
 input.addEventListener('input', debounce(onFormInput, DEBOUNCE_DELAY));
 
+// ===========================================================
+
 function onFormInput(e) {
   e.preventDefault();
 
@@ -71,3 +73,66 @@ function renderCountryInfo([{ name, flags, capital, population, languages }]) {
 
   countryInfo.insertAdjacentHTML('beforeend', markUpCountryInfo);
 }
+
+// ====================================================================================
+
+// function onFormInput(e) {
+//     e.preventDefault();
+
+//     const inputCountry = input.value.trim();
+//     if (inputCountry === '') {
+//       return;
+//     }
+//     fetchCountries(inputCountry)
+//       .then(country => {
+//         countryList.innerHTML = '';
+//         countryInfo.innerHTML = '';
+//         if (country.length === 1) {
+//           renderCountryInfo(country);
+//         } else if (country.length <= 2 && country.length <= 10) {
+//           renderCountryList(country);
+//         } else if (country.length > 10) {
+//           tooManyMathes();
+//         }
+//       })
+//       .catch(onFetchError);
+//   }
+
+//   function onFetchError(error) {
+//     Notiflix.Notify.warning('Oops, there is no country with that name');
+//   }
+//   function tooManyMathes() {
+//     Notiflix.Notify.info(
+//       'Too many matches found. Please enter a more specific name.'
+//     );
+//   }
+
+//   function renderCountryList(country) {
+//     const markUpCountryList = country
+//       .map(({ name, flags }) => {
+//         return `<li class="country-list__new-country">
+//           <img class="country-list__country-flag" src='${flags.svg}' width="150" alt='Flag of ${name.official}'>
+//           <h2 class="country-list__item--name">${name.official}</h2>
+//           </li>`;
+//       })
+//       .join('');
+//     countryList.insertAdjacentHTML('beforeend', markUpCountryList);
+//   }
+
+//   function renderCountryInfo([{ name, flags, capital, population, languages }]) {
+//     const markUpCountryInfo = ` <ul class="country-info__list">
+//               <li class="country-info__item">
+//                 <img class="country-info__item--flag" src="${
+//                   flags.svg
+//                 }" width="150" alt="Flag of ${name.official}">
+//                 <h2 class="country-info__item--name">${name.official}</h2>
+//               </li>
+//               <li class="country-info__item"><span class="country-info__item--categories">Capital: </span>${capital}</li>
+//               <li class="country-info__item"><span class="country-info__item--categories">Population: </span>${population}</li>
+//               <li class="country-info__item"><span class="country-info__item--categories">Languages: </span>${Object.values(
+//                 languages
+//               ).join(', ')}</li>
+//           </ul>`;
+
+//     countryInfo.insertAdjacentHTML('beforeend', markUpCountryInfo);
+//   }
