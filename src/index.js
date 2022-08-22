032f347b8cd939d270a510e9cb6bf0f8a93be9b8
Nikolay Tankov <tankov.n.k@gmail.com>
2022-08-22 22:20:43 +0300

    1

--- src/index.js
+++ src/index.js
@@ -16,14 +16,15 @@ input.addEventListener('input', debounce(onFormInput, DEBOUNCE_DELAY));
 function onFormInput(e) {
   e.preventDefault();
 
+  countryList.innerHTML = '';
+  countryInfo.innerHTML = '';
+
   const inputCountry = input.value.trim();
   if (inputCountry === '') {
     return;
   }
   fetchCountries(inputCountry)
     .then(country => {
-      countryList.innerHTML = '';
-      countryInfo.innerHTML = '';
       if (country.length === 1) {
         renderCountryInfo(country);
       } else if (country.length <= 2 && country.length <= 10) {
@@ -36,11 +37,14 @@ function onFormInput(e) {
 }
 
 function onFetchError(error) {
-  Notiflix.Notify.warning('Oops, there is no country with that name');
+  Notiflix.Notify.warning('Oops, there is no country with that name', {
+    position: 'center-top',
+  });
 }
 function tooManyMathes() {
   Notiflix.Notify.info(
-    'Too many matches found. Please enter a more specific name.'
+    'Too many matches found. Please enter a more specific name.',
+    { position: 'center-top' }
   );
 }
 
@@ -73,66 +77,3 @@ function renderCountryInfo([{ name, flags, capital, population, languages }]) {
 
   countryInfo.insertAdjacentHTML('beforeend', markUpCountryInfo);
 }
-
-// ====================================================================================
-
-// function onFormInput(e) {
-//     e.preventDefault();
-
-//     const inputCountry = input.value.trim();
-//     if (inputCountry === '') {
-//       return;
-//     }
-//     fetchCountries(inputCountry)
-//       .then(country => {
-//         countryList.innerHTML = '';
-//         countryInfo.innerHTML = '';
-//         if (country.length === 1) {
-//           renderCountryInfo(country);
-//         } else if (country.length <= 2 && country.length <= 10) {
-//           renderCountryList(country);
-//         } else if (country.length > 10) {
-//           tooManyMathes();
-//         }
-//       })
-//       .catch(onFetchError);
-//   }
-
-//   function onFetchError(error) {
-//     Notiflix.Notify.warning('Oops, there is no country with that name');
-//   }
-//   function tooManyMathes() {
-//     Notiflix.Notify.info(
-//       'Too many matches found. Please enter a more specific name.'
-//     );
-//   }
-
-//   function renderCountryList(country) {
-//     const markUpCountryList = country
-//       .map(({ name, flags }) => {
-//         return `<li class="country-list__new-country">
-//           <img class="country-list__country-flag" src='${flags.svg}' width="150" alt='Flag of ${name.official}'>
-//           <h2 class="country-list__item--name">${name.official}</h2>
-//           </li>`;
-//       })
-//       .join('');
-//     countryList.insertAdjacentHTML('beforeend', markUpCountryList);
-//   }
-
-//   function renderCountryInfo([{ name, flags, capital, population, languages }]) {
-//     const markUpCountryInfo = ` <ul class="country-info__list">
-//               <li class="country-info__item">
-//                 <img class="country-info__item--flag" src="${
-//                   flags.svg
-//                 }" width="150" alt="Flag of ${name.official}">
-//                 <h2 class="country-info__item--name">${name.official}</h2>
-//               </li>
-//               <li class="country-info__item"><span class="country-info__item--categories">Capital: </span>${capital}</li>
-//               <li class="country-info__item"><span class="country-info__item--categories">Population: </span>${population}</li>
-//               <li class="country-info__item"><span class="country-info__item--categories">Languages: </span>${Object.values(
-//                 languages
-//               ).join(', ')}</li>
-//           </ul>`;
-
-//     countryInfo.insertAdjacentHTML('beforeend', markUpCountryInfo);
-//   }
