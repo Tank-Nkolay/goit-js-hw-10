c139a3772936da17f33e87aadcbe1b54029539c9
Nikolay Tankov <tankov.n.k@gmail.com>
2022-08-23 15:39:35 +0300

    3

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
-import { fetchCountries } from './fetchCountries.js';
+import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
 
