177d42d3f580039cba36a69c2665978c42c67b91
Nikolay Tankov <tankov.n.k@gmail.com>
2022-08-23 16:00:19 +0300

    5

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,5 +1,5 @@
 export function fetchCountries(name) {
-  const baseUrl = 'https:restcountries.com/v3.1/name/';
+  const baseUrl = 'https://restcountries.com/v3.1/name/';
   const ourFilter = '?fields=name,capital,population,flags,languages';
   return fetch(`${baseUrl}${name}${ourFilter}`).then(response => {
     if (!response.ok) {
